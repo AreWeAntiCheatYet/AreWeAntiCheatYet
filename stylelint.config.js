92dd88222bc4be8763918c2541f188626c65cd5b
Starz0r <starz0r@starz0r.com>
2021-10-08 21:19:11 -0500

    Initial commit

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier'
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {}
+}
