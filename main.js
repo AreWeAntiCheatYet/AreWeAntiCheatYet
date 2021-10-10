86ed2ea7a280b8cab616269ddb6ada642a7e4453
Dario <dario@pichowsky.de>
2021-10-10 20:23:11 +0200

    implement logic to read the table

---
+++ main.js
@@ -0,0 +1,39 @@
+const beLogoPath = 'src/assets/battleye-logo.png';
+const eacLogoPath = 'src/assets/easy-logo.png';
+
+fetch("games.json")
+    .then(response => response.json())
+    .then(gamesList => {
+        for (let i = 0; i < gamesList.length; i++) {
+            const game = gamesList[i];
+            const tableBody = document.getElementById('table-body');
+            const highlight = game.acStatus.includes("🎉 Confirmed") ? 'class="is-confirmed"' : '';
+        
+            // template anti-cheats with a logo
+            switch (game.acName) {
+                case "BattlEye":
+                    game.acName = `<img src="${beLogoPath}" width="32" height="32"/> BattlEye`;
+                break;
+        
+                case "Easy Anti-Cheat":
+                    game.acName = `<img src="${eacLogoPath}" width="32" height="32"/> Easy Anti-Cheat`;
+                break;
+        
+                default:
+                    break;
+            }
+        
+            // link to the status url
+            if (game.acStatusUrl !== "") {
+                game.acStatus = `<a href="` + game.acStatusUrl + `">` + game.acStatus + `</a>`;
+            }
+        
+            tableBody.innerHTML += `
+                <tr draggable="false" ${highlight}>
+                    <td><span>${game.game}</span></td>
+                    <td><span>${game.acName}</span></td>
+                    <td><span>${game.acStatus}</span></td>
+                </tr>
+            `;
+    };
+});
