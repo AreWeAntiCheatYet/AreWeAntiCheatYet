1deb5573c1aa961d173cde527324c3ebda1d6488
Ronak Sanpui <ronaksanpui@gmail.com>
2021-10-15 00:39:18 +0530

    did the progress bar

--- src/components/OverView.vue
+++ src/components/OverView.vue
@@ -1,11 +1,28 @@
 <template>
+    <div class="rows is-mobile is-centered">
 
-    <div class="columns is-mobile is-centered" >
-        
-        <p class="pill isConfirmed">🎉 Confirmed {{noConfirmed}}</p>
-        <p class="pill isSupported" >⭐ Supported: {{noSupported}}</p>
-        <p class="pill isUnconfirmed">❔ Unconfirmed: {{noUnconfirmed}}</p>
-        <p class="pill isTotal">📈 Total: {{noConfirmed+noSupported+noUnconfirmed}}</p>
+        <div class="row is-mobile is-centered">
+            <div class="columns is-mobile is-centered" >
+                <p class="pill isSupported" >⭐ Supported: {{noSupported}}</p>
+                <p class="pill isConfirmed">🎉 Confirmed {{noConfirmed}}</p>
+                <p class="pill isUnconfirmed">❔ Unconfirmed: {{noUnconfirmed}}</p>
+                <p class="pill isTotal">📈 Total: {{total}}</p>
+            </div>
+        </div>
+
+        <div class="row is-mobile is-centered">
+            <div class="columns is-mobile is-centered" >
+
+            <b-progress format="percent" :max="100" size="is-large">
+            <template #bar>
+                <b-progress-bar :value="supportedPercent" type="is-success" show-value></b-progress-bar>
+                <b-progress-bar :value="conmfirmedPercent" type="is-info" show-value></b-progress-bar>
+                <b-progress-bar :value="unconfirmedPercent" type="" show-value></b-progress-bar>
+            </template>
+            </b-progress>
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -13,10 +30,21 @@
 
 
 export default {
-    props:['noUnconfirmed','noSupported','noConfirmed']
+    props:['noUnconfirmed','noSupported','noConfirmed'],
+    data(){
+        const total=this.noUnconfirmed+this.noSupported+this.noConfirmed;
+        
+        return {
+            total,
+            conmfirmedPercent:this.noConfirmed/total*100,
+            supportedPercent:this.noSupported/total*100,
+            unconfirmedPercent:this.noUnconfirmed/total*100,
+            };
+    }
 }
 </script>
 <style>
+
     .pill{
         padding: 0.3rem 1rem 0.2rem 1rem;
         font-size: 1.5rem;
@@ -41,6 +69,9 @@ export default {
     }
     .isTotal{
         background: rgb(255, 174, 0);
+    }
+    .row{
+        margin: 1rem 0 5rem 0;
     }
 
 </style>
