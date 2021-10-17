87a4e11f86f5555a84287d9c625d67de37c380ef
PyDario <dario@pichowsky.de>
2021-10-17 18:06:14 +0200

    Revert "remove all unecessary vue and nuxt stuff"
    This reverts commit 499e7a6a0c8f799cc8559c516171affd8e082ce6.

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier'
+  ],
+  ignoreFiles: [
+	'./src/assets/css/sandstone.css'
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {}
+}
