3c3ca5fe919d078c194f8e479dfd568f4a21f2b3
Curve <fynnbwdt@gmail.com>
2022-05-24 12:19:14 +0200

    feat: add project description

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,23 @@
-import { ActionIcon, Avatar, Group, Header, Title } from '@mantine/core';
+import { ActionIcon, Avatar, Group, Header, Text, Title } from '@mantine/core';
 import { IconBrandGithub } from '@tabler/icons';
+import { style } from '../utils/style';
 import ThemeToggle from './ThemeToggle';
 
 export default function AppHeader() {
+  const { classes } = style();
+
   return (
     <Header height={60}>
       <Group position="apart" sx={{ marginLeft: 15, marginRight: 15, height: '100%' }} noWrap>
-        <Group position="center">
+        <Group position="center" noWrap>
           <Avatar src="icon.webp" size="lg" />
           <Title order={4}>Are We Anti-Cheat Yet?</Title>
         </Group>
-        <Group position="center">
+        <Text className={classes.smallHide}>
+          A comprehensive and crowd-sourced list of games using anti-cheats and their compatibility
+          with GNU/Linux or Wine/Proton.{' '}
+        </Text>
+        <Group position="center" noWrap>
           <ActionIcon
             variant="light"
             color="violet"
