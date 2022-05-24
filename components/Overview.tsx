aa6ef2f218f00e400948420ddf583f1113574b5d
Curve <fynnbwdt@gmail.com>
2022-05-24 11:36:52 +0200

    feat(overview): add percentage

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -14,16 +14,17 @@ interface LegendProps {
   name: string;
   color: string;
   amount: number;
+  percentage: number;
   description: string;
 }
 
-function Legend({ name, color, amount, description }: LegendProps) {
+function Legend({ name, color, amount, percentage, description }: LegendProps) {
   return (
     <Tooltip withArrow label={description}>
       <Group align="center">
         <ColorSwatch color={color} />
         <Text>
-          {name} ({amount})
+          {amount} {name} ({percentage.toFixed(1)}%)
         </Text>
       </Group>
     </Tooltip>
@@ -57,30 +58,35 @@ export default function Overview({ overview, ...props }: OverviewProps) {
           color={theme.colors.green[6]}
           amount={overview.supported}
           description="Game is officially supported"
+          percentage={(overview.supported / overview.total) * 100}
         />
         <Legend
           name="Planned"
           color={theme.colors.violet[6]}
           amount={overview.planned}
           description="Game plans to support Proton/Wine"
+          percentage={(overview.planned / overview.total) * 100}
         />
         <Legend
           name="Running"
           color={theme.colors.cyan[6]}
           amount={overview.running}
           description="No official statement but runs fine (may require tinkering)"
+          percentage={(overview.running / overview.total) * 100}
         />
         <Legend
           name="Broken"
           color={theme.colors.orange[6]}
           amount={overview.broken}
           description="Game does not work (online)"
+          percentage={(overview.broken / overview.total) * 100}
         />
         <Legend
           name="Denied"
           color={theme.colors.red[6]}
           amount={overview.denied}
           description="Linux support was explicitly denied"
+          percentage={(overview.denied / overview.total) * 100}
         />
       </Stack>
     </Group>
