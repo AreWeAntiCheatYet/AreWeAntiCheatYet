dfd279842a23c2729747b98a56dee6effa22a0d9
Curve <fynnbwdt@gmail.com>
2022-05-28 14:30:10 +0200

    fix(buld): increase generation timeout

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
+  staticPageGenerationTimeout: 500,
   eslint: {
     ignoreDuringBuilds: true,
   },
