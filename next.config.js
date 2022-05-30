dce1e6cd788113a518b5d54b4e9ac7740b188049
Curve <fynnbwdt@gmail.com>
2022-05-30 17:22:09 +0200

    chore: increase page generation timeout

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-  staticPageGenerationTimeout: 500,
+  staticPageGenerationTimeout: 1500,
   eslint: {
     ignoreDuringBuilds: true,
   },
