e97c020c3dad06fb81fbdd7df6d009e011727585
D3SOX <nico@d3sox.me>
2022-08-03 17:16:12 +0200

    fix: migrate toggles for Tooltip
    See https://mantine.dev/core/tooltip/#required-ref-prop

--- components/ShowStoresToggle.tsx
+++ components/ShowStoresToggle.tsx
@@ -1,13 +1,14 @@
 import { ActionIcon } from '@mantine/core';
 import { IconShoppingCart, IconShoppingCartOff } from '@tabler/icons';
+import { forwardRef } from 'react';
 
 interface ShowStoresToggleProps {
   showStores: boolean;
   toggleShowStores: () => void;
 }
 
-export default function ShowStoresToggle({ showStores, toggleShowStores }: ShowStoresToggleProps) {
-  return (
+const ShowStoresToggle = forwardRef<HTMLButtonElement, ShowStoresToggleProps>(
+  ({ showStores, toggleShowStores, ...props }, ref) => (
     <ActionIcon
       onClick={toggleShowStores}
       size="lg"
@@ -16,8 +17,12 @@ export default function ShowStoresToggle({ showStores, toggleShowStores }: ShowS
         backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
         color: showStores ? theme.colors.green[6] : theme.colors.gray[4],
       })}
+      ref={ref}
+      {...props}
     >
       {showStores ? <IconShoppingCart size={18} /> : <IconShoppingCartOff size={18} />}
     </ActionIcon>
-  );
-}
+  )
+);
+
+export default ShowStoresToggle;
