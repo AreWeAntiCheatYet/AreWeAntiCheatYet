d4a1dc36000f15be291f8ec8781cea2eaae48156
Curve <fynnbwdt@gmail.com>
2022-08-30 13:05:14 +1000

    refactor: remove logo .gitignore from repo, change Image to img
    Image -> img is done because they apparently don't work in static builds

--- components/GamesList.tsx
+++ components/GamesList.tsx
@@ -92,7 +92,7 @@ function Item({ game, highlight, showStores, anticheatIcons }: ItemProps) {
                   target="_blank"
                   href={`https://store.epicgames.com/p/${game.storeIds.epic.slug}/`}
                 >
-                  <Image width="18" height="18" layout="fixed" src="/stores/epic-games.webp" />
+                  <img alt="epic-store" width="18" height="18" src="/stores/epic-games.webp" />
                 </ActionIcon>
               )}
               {game.storeIds.steam && (
