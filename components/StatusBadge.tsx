d223667ea04a85ca8ff0b5a2cadf2a03ac43757b
Curve <fynnbwdt@gmail.com>
2023-02-09 19:05:50 +0100

    feat(components): add statusbadge

---
+++ components/StatusBadge.tsx
@@ -0,0 +1,25 @@
+import { Card, CardProps, Group, SpacingValue, SystemProp, Text } from '@mantine/core';
+import { Game } from '../src/types/games';
+import { getStyle } from '../src/utils/games';
+
+interface StatusBadgeProps extends Omit<CardProps, 'bg' | 'children'> {
+  fz?: SystemProp<SpacingValue>;
+  weight?: number;
+  size?: number;
+  game: Game;
+}
+
+export default function ({ game, radius, size, fz, weight, ...props }: StatusBadgeProps) {
+  const status = getStyle(game.status);
+
+  return (
+    <Card bg={status.color} radius={radius || 'xl'} {...props}>
+      <Group noWrap>
+        <status.icon color="white" size={size} />
+        <Text fz={fz} weight={weight} color="white" align="center">
+          {game.status}
+        </Text>
+      </Group>
+    </Card>
+  );
+}
