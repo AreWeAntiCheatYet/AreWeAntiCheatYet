f2e13206334431744401aa7fb94e996d91282a4e
Curve <fynnbwdt@gmail.com>
2023-02-09 19:05:24 +0100

    feat: add anticheat badge

---
+++ components/AntiCheatBadge.tsx
@@ -0,0 +1,24 @@
+import { ThemeIcon, Tooltip, TooltipProps } from '@mantine/core';
+import { IconQuestionMark } from '@tabler/icons-react';
+import { getLogo } from '../src/utils/games';
+
+interface AntiCheatBadgeProps extends Omit<TooltipProps, 'transition' | 'events' | 'children' | 'label'> {
+  height: number;
+  anticheat: string;
+}
+
+export default function ({ anticheat, height, ...props }: AntiCheatBadgeProps) {
+  const logo = getLogo(anticheat);
+
+  return (
+    <Tooltip {...props} label={anticheat} transition="slide-up" events={{ hover: true, touch: true, focus: true }}>
+      {logo ? (
+        <img src={logo} alt={anticheat} height={height} />
+      ) : (
+        <ThemeIcon color="gray" radius="xl" size={height}>
+          <IconQuestionMark />
+        </ThemeIcon>
+      )}
+    </Tooltip>
+  );
+}
