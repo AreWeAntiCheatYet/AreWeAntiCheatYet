88ebc03e8c57923870ca0d7a321bfb16708f2152
Curve <fynnbwdt@gmail.com>
2023-02-11 16:31:27 +0100

    feat(shell): make logo & title navigate to home page

--- components/Shell.tsx
+++ components/Shell.tsx
@@ -1,6 +1,7 @@
 import { ActionIcon, AppShell, Group, Header, Image, Title, useMantineTheme } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 import { IconSettings } from '@tabler/icons-react';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import Settings from './Settings';
 import ThemeToggle from './ThemeToggle';
@@ -8,13 +9,14 @@ import ThemeToggle from './ThemeToggle';
 function Head({ setSettings }: { setSettings: (enabled: boolean) => void }) {
   const breakpoint = useMediaQuery('(min-width: 1200px)');
   const theme = useMantineTheme();
+  const router = useRouter();
 
   const background = theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[2];
 
   return (
     <Header height={60} fixed bg={background}>
       <Group px={15} h={60} position="apart" noWrap>
-        <Group position="center" noWrap>
+        <Group sx={{ cursor: 'pointer' }} onClick={() => router.push('/')} position="center" noWrap>
           <Image src="/icon.webp" width={64} height={64} />
           {breakpoint && (
             <Title ml={-10} order={2}>
