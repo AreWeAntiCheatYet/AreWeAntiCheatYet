9d784201b6616392617133f7684722061b03d41b
Curve <fynnbwdt@gmail.com>
2023-02-11 17:23:51 +0100

    feat: add display setting

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,5 +1,9 @@
+import Games from '../../games.json';
+
 export interface Settings {
+  previousGames: string;
+  display: 'table' | 'grid';
   overview: 'simple' | 'detailed' | 'ring';
 }
 
-export const defaultSettings: Settings = { overview: 'ring' };
+export const defaultSettings: Settings = { overview: 'ring', display: 'table', previousGames: JSON.stringify(Games) };
