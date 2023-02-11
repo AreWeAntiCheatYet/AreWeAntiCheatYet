9e80d0e588a6ceeb690ab11e72ca8c1f99b4a566
Curve <fynnbwdt@gmail.com>
2023-02-11 19:15:04 +0100

    feat(settings): add display and previousGames

--- src/app/state.tsx
+++ src/app/state.tsx
@@ -2,7 +2,9 @@ import { createContext, ReactNode, useEffect, useState } from 'react';
 import { defaultSettings, Settings } from '../types/settings';
 
 interface SettingsSetter {
+  setDisplay: (arg: Settings['display']) => void;
   setOverview: (arg: Settings['overview']) => void;
+  setPreviousGames: (arg: Settings['previousGames']) => void;
 }
 
 const update = <C extends keyof Settings>(name: C, set: (v: Settings[C]) => void) => {
@@ -19,14 +21,27 @@ const get = <C extends keyof Settings>(name: C, set: (v: Settings[C]) => void) =
 export const SettingsContext = createContext<Settings & SettingsSetter>(null);
 
 export function SettingsProvider({ children }: { children: ReactNode }) {
+  const [previousGames, setPreviousGames] = useState(defaultSettings.previousGames);
   const [overview, setOverview] = useState(defaultSettings.overview);
+  const [display, setDisplay] = useState(defaultSettings.display);
 
   useEffect(() => {
+    get('display', setDisplay);
     get('overview', setOverview);
+    get('previousGames', setPreviousGames);
   }, []);
 
   return (
-    <SettingsContext.Provider value={{ overview, setOverview: update('overview', setOverview) }}>
+    <SettingsContext.Provider
+      value={{
+        display,
+        setDisplay: update('display', setDisplay),
+        overview,
+        setOverview: update('overview', setOverview),
+        previousGames,
+        setPreviousGames: update('previousGames', setPreviousGames),
+      }}
+    >
       {children}
     </SettingsContext.Provider>
   );
