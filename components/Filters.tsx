658b3b9d1f6445c70dafbd5f647b6a5e4b45f091
Curve <fynnbwdt@gmail.com>
2023-02-13 20:09:45 +0100

    feat: encode filters in url

---
+++ components/Filters.tsx
@@ -0,0 +1,114 @@
+import { Select, TextInput } from '@mantine/core';
+import { useDebouncedValue } from '@mantine/hooks';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { paginationSize } from '../src/static';
+import { Game } from '../src/types/games';
+import { filter, paginate, sort } from '../src/utils/games';
+
+export default function ({
+  page,
+  games,
+  setGames,
+  setFiltered,
+  initialGames,
+}: {
+  page: number;
+  games: Game[];
+  initialGames: Game[];
+  setGames: (v: Game[]) => void;
+  setFiltered: (v: boolean) => void;
+}) {
+  const router = useRouter();
+
+  const [rawSearch, setSearch] = useState('');
+  const [search] = useDebouncedValue(rawSearch, 200);
+
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>(undefined);
+  const [sortBy, setSortBy] = useState<'name' | 'status' | 'updates'>(undefined);
+
+  useEffect(() => {
+    const { searchParams } = new URL(window.location.href);
+
+    setSearch(searchParams.get('search'));
+    setSortBy(searchParams.get('sortBy') as typeof sortBy);
+    setSortOrder(searchParams.get('sortOrder') as typeof sortOrder);
+  }, []);
+
+  useEffect(() => {
+    router.replace(
+      {
+        query: { ...router.query, search, sortOrder, sortBy },
+      },
+      undefined,
+      { scroll: false },
+    );
+  }, [page, search, sortOrder, sortBy]);
+
+  useEffect(() => {
+    if (!search) {
+      setGames(initialGames);
+      setFiltered(false);
+      return;
+    }
+
+    setFiltered(true);
+    setGames(filter(search));
+  }, [search]);
+
+  useEffect(() => {
+    if (!search && sortBy === null) {
+      setGames([...initialGames]);
+      return;
+    }
+
+    if (sortBy === undefined) {
+      return;
+    }
+
+    if (search) {
+      const sorted = sort(sortBy, sortOrder, games);
+      setGames([...sorted]);
+      return;
+    }
+
+    const sorted = sort(sortBy, sortOrder);
+    const paginated = paginate(paginationSize, sorted);
+
+    setGames([...paginated.at(page - 1)]);
+  }, [page, sortBy, sortOrder]);
+
+  return (
+    <>
+      <TextInput
+        value={rawSearch}
+        description="Search"
+        placeholder="Name, Status, ..."
+        onChange={(value) => setSearch(value.currentTarget.value)}
+      />
+      <Select
+        description="Sort By"
+        placeholder="Nothing"
+        clearable
+        data={[
+          { value: 'name', label: 'Name' },
+          { value: 'status', label: 'Status' },
+          { value: 'updates', label: 'Updates' },
+        ]}
+        value={sortBy}
+        onChange={(value) => setSortBy(value as Parameters<typeof setSortBy>[0])}
+      />
+      <Select
+        description="Sort Order"
+        placeholder="Nothing"
+        clearable
+        data={[
+          { value: 'asc', label: 'Ascending' },
+          { value: 'desc', label: 'Descending' },
+        ]}
+        value={sortOrder}
+        onChange={(value) => setSortOrder(value as Parameters<typeof setSortOrder>[0])}
+      />
+    </>
+  );
+}
--- components/GameGrid.tsx
+++ components/GameGrid.tsx
@@ -1,11 +1,10 @@
-import { Group, Pagination, Select, SimpleGrid, SimpleGridProps, Stack, TextInput } from '@mantine/core';
-import { useDebouncedState, useMediaQuery } from '@mantine/hooks';
+import { Group, Pagination, SimpleGrid, SimpleGridProps, Stack } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { useRouter } from 'next/dist/client/router';
-import { useEffect, useState } from 'react';
-import { paginationSize } from '../src/static';
+import { useState } from 'react';
 import { Asset } from '../src/types/assets';
 import { Game } from '../src/types/games';
-import { filter, paginate, sort } from '../src/utils/games';
+import Filters from './Filters';
 import GameCard from './GameCard';
 
 interface GameGridProps extends Omit<SimpleGridProps, 'cols' | 'spacing' | 'breakpoints'> {
@@ -15,91 +14,6 @@ interface GameGridProps extends Omit<SimpleGridProps, 'cols' | 'spacing' | 'brea
   page: number;
 }
 
-function Filters({
-  page,
-  games,
-  setGames,
-  setFiltered,
-  initialGames,
-}: {
-  page: number;
-  games: Game[];
-  initialGames: Game[];
-  setGames: (v: Game[]) => void;
-  setFiltered: (v: boolean) => void;
-}) {
-  const [search, setSearch] = useDebouncedState('', 200);
-
-  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>(undefined);
-  const [sortBy, setSortBy] = useState<'name' | 'status' | 'updates'>(undefined);
-
-  useEffect(() => {
-    if (!search) {
-      setGames(initialGames);
-      setFiltered(false);
-      return;
-    }
-
-    setFiltered(true);
-    setGames(filter(search));
-  }, [search]);
-
-  useEffect(() => {
-    if (sortBy === undefined) {
-      return;
-    }
-
-    if (!search && sortBy === null) {
-      setGames([...initialGames]);
-      return;
-    }
-
-    if (search) {
-      const sorted = sort(sortBy, sortOrder, games);
-      setGames([...sorted]);
-      return;
-    }
-
-    const sorted = sort(sortBy, sortOrder);
-    const paginated = paginate(paginationSize, sorted);
-
-    setGames([...paginated.at(page - 1)]);
-  }, [page, sortBy, sortOrder]);
-
-  return (
-    <Group mb={10} noWrap>
-      <TextInput
-        description="Search"
-        placeholder="Name, Status, ..."
-        onChange={(value) => setSearch(value.currentTarget.value)}
-      />
-      <Select
-        description="Sort By"
-        placeholder="Nothing"
-        clearable
-        data={[
-          { value: 'name', label: 'Name' },
-          { value: 'status', label: 'Status' },
-          { value: 'updates', label: 'Updates' },
-        ]}
-        value={sortBy}
-        onChange={(value) => setSortBy(value as Parameters<typeof setSortBy>[0])}
-      />
-      <Select
-        description="Sort Order"
-        placeholder="Nothing"
-        clearable
-        data={[
-          { value: 'asc', label: 'Ascending' },
-          { value: 'desc', label: 'Descending' },
-        ]}
-        value={sortOrder}
-        onChange={(value) => setSortOrder(value as Parameters<typeof setSortOrder>[0])}
-      />
-    </Group>
-  );
-}
-
 export default function ({ page, games, totalPages, assets, ...props }: GameGridProps) {
   const breakpoint = useMediaQuery('(min-width: 1200px)') ?? true;
   const [filteredGames, setGames] = useState(games);
@@ -107,8 +21,10 @@ export default function ({ page, games, totalPages, assets, ...props }: GameGrid
   const router = useRouter();
 
   return (
-    <Stack align="center" mt={20} mb={20}>
-      <Filters games={filteredGames} page={page} initialGames={games} setFiltered={setFiltered} setGames={setGames} />
+    <Stack align="center" mt={50} mb={20}>
+      <Group noWrap>
+        <Filters games={filteredGames} page={page} initialGames={games} setFiltered={setFiltered} setGames={setGames} />
+      </Group>
       <SimpleGrid
         spacing="xl"
         cols={4}
