7511712d0c01e12a5819417afc8269dda64a9878
Curve <fynnbwdt@gmail.com>
2023-02-17 13:58:27 +0100

    refactor: update breakdown link

--- components/BreakdownLink.tsx
+++ components/BreakdownLink.tsx
@@ -1,17 +1,16 @@
 import { Button, ButtonProps } from '@mantine/core';
 import { IconChartArea } from '@tabler/icons-react';
-import { useRouter } from 'next/router';
 
 export default function ({ ...props }: Omit<ButtonProps, 'color' | 'leftIcon' | 'variant' | 'onClick'>) {
-  const router = useRouter();
-
   return (
     <Button
       {...props}
       color="gray"
+      component="a"
+      target="_blank"
       variant="light"
+      href="/breakdown"
       leftIcon={<IconChartArea />}
-      onClick={() => router.push('/breakdown')}
     >
       Anti-Cheat Breakdown
     </Button>
