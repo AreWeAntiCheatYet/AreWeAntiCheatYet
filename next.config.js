423a69c8e86f720928f2d8e1927ed6fdbef0ceb9
Curve <fynnbwdt@gmail.com>
2023-02-17 16:17:42 +0100

    fix(workflows): deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
 module.exports = withBundleAnalyzer({
-  assetPrefix: process.env.PAGES ? '/AreWeAntiCheatYet' : undefined,
-  basePath: process.env.PAGES ? '/AreWeAntiCheatYet' : undefined,
+  assetPrefix: isGithubActions ? '/AreWeAntiCheatYet' : undefined,
+  basePath: isGithubActions ? '/AreWeAntiCheatYet' : undefined,
   images: { unoptimized: true },
   reactStrictMode: true,
 });
