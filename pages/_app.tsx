3d92b44a4a34cbd93b39d057ee6ea421f9a7a5df
Starz0r <starz0r@starz0r.com>
2023-04-12 22:32:57 -0500

    pages/app: NotificationProvider is now Notifications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
-import { NotificationsProvider } from '@mantine/notifications';
+import { Notifications } from '@mantine/notifications';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -12,51 +12,51 @@ import { SettingsProvider } from '../src/static/state';
 import theme from '../src/static/theme';
 
 export default function App(props: AppProps) {
-  const { basePath } = useRouter();
-  const { Component, pageProps } = props;
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+    const { basePath } = useRouter();
+    const { Component, pageProps } = props;
+    const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
 
-  const toggleColorScheme = (value?: ColorScheme) => {
-    const scheme = value || (colorScheme == 'dark' ? 'light' : 'dark');
-    localStorage.setItem('theme', scheme);
-    setColorScheme(scheme);
-  };
+    const toggleColorScheme = (value?: ColorScheme) => {
+        const scheme = value || (colorScheme == 'dark' ? 'light' : 'dark');
+        localStorage.setItem('theme', scheme);
+        setColorScheme(scheme);
+    };
 
-  useEffect(() => {
-    toggleColorScheme((localStorage.getItem('theme') as ColorScheme) || 'dark');
-  }, []);
+    useEffect(() => {
+        toggleColorScheme((localStorage.getItem('theme') as ColorScheme) || 'dark');
+    }, []);
 
-  useEffect(() => {
-    document.documentElement.style.colorScheme = colorScheme;
-  }, [colorScheme]);
+    useEffect(() => {
+        document.documentElement.style.colorScheme = colorScheme;
+    }, [colorScheme]);
 
-  return (
-    <>
-      <Head>
-        <title>Are We Anti-Cheat Yet?</title>
-        <link rel="shortcut icon" href={`${basePath}/icon.webp`} />
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="ShortName" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Are We Anti-Cheat Yet?</title>
+                <link rel="shortcut icon" href={`${basePath}/icon.webp`} />
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+                <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="ShortName" />
+            </Head>
 
-      <Script
-        src="https://polyfill.io/v3/polyfill.min.js?features=Array.prototype.at%2Cdefault"
-        strategy="beforeInteractive"
-      />
+            <Script
+                src="https://polyfill.io/v3/polyfill.min.js?features=Array.prototype.at%2Cdefault"
+                strategy="beforeInteractive"
+            />
 
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider theme={{ colorScheme, ...theme }} withGlobalStyles withNormalizeCSS>
-          <NotificationsProvider>
-            <SettingsProvider>
-              <ModalsProvider>
-                <Shell>
-                  <Component {...pageProps} />
-                </Shell>
-              </ModalsProvider>
-            </SettingsProvider>
-          </NotificationsProvider>
-        </MantineProvider>
-      </ColorSchemeProvider>
-    </>
-  );
+            <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+                <MantineProvider theme={{ colorScheme, ...theme }} withGlobalStyles withNormalizeCSS>
+                    <Notifications>
+                        <SettingsProvider>
+                            <ModalsProvider>
+                                <Shell>
+                                    <Component {...pageProps} />
+                                </Shell>
+                            </ModalsProvider>
+                        </SettingsProvider>
+                    </Notifications>
+                </MantineProvider>
+            </ColorSchemeProvider>
+        </>
+    );
 }
