c85f95165e81944de06af797ba76466692e33f98
Starz0r <starz0r@starz0r.com>
2024-01-18 23:19:00 -0600

    types/games: date changed field
    This is mainly for the editor, eventually we'll be able to do fearless merges with this little bit of info.

--- src/types/games.ts
+++ src/types/games.ts
@@ -1,39 +1,41 @@
 export type Status = 'Broken' | 'Running' | 'Denied' | 'Supported' | 'Planned';
 
 export interface Update {
-  name: string;
-  date: string;
+    name: string;
+    date: string;
 
-  reference: string;
+    reference: string;
 }
 
 export interface StoreIds {
-  epic?: {
-    namespace: string;
-    slug: string;
-  };
-  steam?: string;
+    epic?: {
+        namespace: string;
+        slug: string;
+    };
+    steam?: string;
 }
 
 export interface Game {
-  url: string;
-  slug: string;
-  name: string;
-  logo: string;
-  native: boolean;
+    url: string;
+    slug: string;
+    name: string;
+    logo: string;
+    native: boolean;
 
-  status: Status;
-  reference: string;
-  anticheats: string[];
+    status: Status;
+    reference: string;
+    anticheats: string[];
 
-  updates: Update[];
-  notes: [text: string, reference: string][];
+    updates: Update[];
+    notes: [text: string, reference: string][];
 
-  storeIds: StoreIds;
+    storeIds: StoreIds;
+
+    dateChanged: Date;
 }
 
 export interface Change {
-  old?: Game;
-  recent: Game;
-  changedProperties?: (keyof Game)[];
+    old?: Game;
+    recent: Game;
+    changedProperties?: (keyof Game)[];
 }
