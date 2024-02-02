ec2069e6d3308b6494f25a1742aa5738135bf950
Starz0r <starz0r@starz0r.com>
2024-02-01 22:57:33 -0600

    components/BannerRadio: adapt to new mantine changes

--- components/BannerRadio.tsx
+++ components/BannerRadio.tsx
@@ -1,27 +1,28 @@
-import { Card, Radio, RadioProps, Stack, Text, useMantineTheme } from '@mantine/core';
+import { Card, Radio, RadioProps, Stack, Text, useMantineColorScheme, useMantineTheme } from '@mantine/core';
 
 interface BannerRadioProps extends Omit<RadioProps, 'label'> {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
-export default function ({ description, children, ...props }: BannerRadioProps) {
-  const theme = useMantineTheme();
-  const background = theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[3];
+export default function({ description, children, ...props }: BannerRadioProps) {
+    const theme = useMantineTheme();
+    const { colorScheme } = useMantineColorScheme();
+    const background = colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[3];
 
-  return (
-    <Card
-      withBorder
-      style={{
-        border: props.checked ? `2px solid ${theme.colors[theme.primaryColor][6]}` : undefined,
-      }}
-    >
-      <Card.Section bg={background}>{children}</Card.Section>
-      <Stack mt={10} align="center">
-        <Text fz="md" color="dimmed" align="center">
-          {description}
-        </Text>
-        <Radio {...props} />
-      </Stack>
-    </Card>
-  );
+    return (
+        <Card
+            withBorder
+            style={{
+                border: props.checked ? `2px solid ${theme.colors[theme.primaryColor][6]}` : undefined,
+            }}
+        >
+            <Card.Section bg={background}>{children}</Card.Section>
+            <Stack mt={10} align="center">
+                <Text fz="md" c="dimmed" style={{ textAlign: "center" }}>
+                    {description}
+                </Text>
+                <Radio {...props} />
+            </Stack>
+        </Card>
+    );
 }
