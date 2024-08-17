bc6fbe3f8cc2a9a3a7ca1cd4ed4a40224a803def
Asir N. <an739043@sju.edu>
2024-08-17 16:20:58 -0400

    games: remove unnecessary stuff.

--- src/utils/games.ts
+++ src/utils/games.ts
@@ -111,7 +111,6 @@ export function getLogo(anticheat: string) {
     ['seasun','seasun.webp'],
     ['wfsdrv','wfsdrv.webp'],
     ['violet','violet.webp'],
-    ['frost','frost.webp'],
   ];
 
   const file = logo_map.find((x) => anticheat.toLowerCase().includes(x[0]))?.[1];
